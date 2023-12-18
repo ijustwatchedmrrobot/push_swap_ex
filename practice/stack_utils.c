7b011f42a09c1fa95fa847ff82c3e07b7868a9f7
Bora Özdamar <sboraozdamar@gmail.com>
2023-12-18 11:03:18 +0300

    .

--- practice/stack_utils.c
+++ practice/stack_utils.c
@@ -1,24 +1,40 @@
-
+#include "../libft/libft.h"
 
 typedef struct stack_list
 {
 	int					data;
 	int					index;
 	struct stack_list	*next;
-} stack;
+} Stack;
 
-stack	*create_stack()
+void	push(Stack top, int data)
 {
-	
+	Stack	*temp;
+	static int index = 0;
+
+	if (top == NULL)
+	{
+		top->index = -1;
+		top->data = data;
+		top->next = NULL;
+	}
+	else
+	{
+		temp = malloc(sizeof(Stack));
+		temp->data = data;
+		temp->next = top;
+		temp->index = index++;
+		top->next = NULL;
+	}
 }
 
-void	push(stack node, int data)
-{
-
-}
-
-void	fill_stack(int nums, ...)
+void	create_stack(int *arr)
 {
 	stack	*temp;
 	stack	*ptr;
+	int	arr_size;
+
+	arr_size = (sizeof(arr)/4);
+	while (arr_size--)
+		push(nums);
 }
