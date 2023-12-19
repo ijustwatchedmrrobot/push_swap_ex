5e53ecdc69a267bd04b3f2e8bc5728dcf80fcfd5
Bora Özdamar <sboraozdamar@gmail.com>
2023-12-19 22:34:19 +0300

    some utils added, arg funcs problem

--- commands.c
+++ commands.c
@@ -118,10 +118,10 @@ int	rotate(t_list	**stack)
 	t_list	*top;
 	t_list	*bot;
 
-	if (ft_lstsize(&stack) < 2)
+	if (ft_lstsize(*stack) < 2)
 		return (-1);
 	top = *stack;
-	bot = ft_lstlast(&stack);
+	bot = ft_lstlast(*stack);
 	*stack = top->next;
 	top->next = NULL;
 	bot->next = top;
@@ -154,3 +154,53 @@ int	rr(t_list	**stack_a, t_list	**stack_b)
 	return (0);
 }
 
+//Shift all the elements of the stack by one in the reverse direction
+// NULL <- 1 <- 2 <- 3 <- 4 <- 5 <- *stack last becomes top
+// NULL <- NULL <- 2 <- 3 <- 4 <- 5 <- *stack
+// NULL <- 2 <- 3 <- 4 <- 5 <- 1 <- *stack
+int	reverse_rotate(t_list	**stack)
+{
+	t_list	*top;
+	t_list	*bot;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	top = *stack;
+	bot = ft_lstlast(*stack);
+	while (top)
+	{
+		if (top->next->next == NULL)
+		{
+			top->next = NULL;
+			break;
+		}
+		top = top->next;
+	}
+	bot->next = *stack;
+	*stack = bot;
+	return (0);
+}
+
+int	rra(t_list **stack_a)
+{	
+	if (reverse_rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("rra", 1);
+	return (0);
+}
+
+int	rrb(t_list **stack_b)
+{	
+	if (reverse_rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rrb", 1);
+	return (0);
+}
+
+int	rrr(t_list **stack_a, t_list **stack_b)
+{
+	if (ft_lstsize(*stack_a) < 2 || ft_lstsize(*stack_b) < 2)
+		return (-1);
+	ft_putendl_fd("rrr", 1);
+	return (0);
+}
--- utils.c
+++ utils.c
@@ -12,8 +12,124 @@
 
 #include "push_swap.h"
 
+
+//utils
 void	ft_error(char *msg)
 {
 	ft_putendl_fd(msg, 1);
 	exit(0);
 }
+
+int		is_sorted(t_list **stack)
+{
+	t_list	*top;
+
+	top = *stack;
+	while (top && top->next)
+	{
+		if (top->data < top->next->data)
+			return (0);
+		top = top->next;
+	}
+	return (1);
+}
+
+void	str_free(char **str)
+{
+	while (*str)
+	{
+		free(*str);
+		str++;
+	}
+}
+
+int	stack_distance(t_list **stack, int index)
+{
+	t_list	*top;
+	int		distance;
+
+	distance = 0;
+	top = *stack;
+	while (top->index != index)
+	{
+		distance++;
+		top = top->next;	
+	}
+	return (distance);
+}
+
+void	free_stack(t_list **stack)
+{
+	t_list	*top;
+	t_list	*temp;
+
+	top = *stack;
+	while (top)
+	{
+		temp = top;
+		top = top->next;
+		free(temp);
+	}
+	free(stack);
+}
+
+void	print_stack(t_list *top)
+{
+	t_list	*temp;
+
+	temp = top;
+	while (temp != NULL)
+	{
+		ft_putnbr_fd(temp->value, 1);
+		ft_putendl_fd("", 1);
+		temp = temp->next;
+	}
+}
+
+
+//try to rm index
+int	contains(int num, char **av, int i)
+{
+	i++;
+	while (*(argv + i))
+	{
+		if (ft_atoi(*(argv + i++)) == num)
+			return (1);
+	}
+	return (0);
+}
+
+int	is_str_num(char *num)
+{
+	if (*num == '-')
+		num++;
+	while (*num)
+	{
+		if (!ft_isdigit(*num++))
+			return (0);
+	}
+	return (1);
+}
+
+void	check_args(int ac, char **av)
+{
+	long	temp;
+	char	**args;
+
+	if (ac == 2)
+		args = ft_split(*(av + 1), ' ');
+	else
+		*(args++) = av;
+	while (*args)
+	{
+		temp = ft_atoi(*args);
+		if (!is_str_num(*args))
+			ft_error("Error! Not a number.");
+		//if (contains)
+		if (temp < -2147483648 || temp > 2147483647)
+			ft_error("Error! Limit for integer exceeded.");
+	
+	}
+}
+
+// char **args =  {"one", "two", "three", "four", "three"}
